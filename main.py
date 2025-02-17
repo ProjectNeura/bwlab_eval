ab6d9af5802527384119632c0450e3fe0758cf98
ATATC <futerry@outlook.com>
2025-02-17 13:16:02 -0500

    Routine updates.

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     parser.add_argument("-tr", "--trainer", default="nnUNetTrainer")
     parser.add_argument("-p", "--plan", default="nnUNetPlans")
     args = parser.parse_args()
-    _run(f"python infer.py -d {args.dataset} -dn {args.dataset_name} -c 3d_fullres -tr {args.trainer} -p {args.plan} -n 13")
-    _run(f"python eval.py --gt_path /workspace/data/nnUNet_raw/Dataset{args.dataset}_{args.dataset_name}/labelsTs --save_path /workspace/data/bwlab_eval_results.csv")
+    _run(f"python /workspace/code/infer.py -d {args.dataset} -dn {args.dataset_name} -c 3d_fullres -tr {args.trainer} -p {args.plan} -n 13")
+    _run(f"python /workspace/code/eval.py --gt_path /workspace/data/nnUNet_raw/Dataset{args.dataset}_{args.dataset_name}/labelsTs --save_path /workspace/data/bwlab_eval_results.csv")
