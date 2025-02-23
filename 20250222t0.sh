da664ccc0d17b66f52332e7da4172242abe68c3a
ATATC <futerry@outlook.com>
2025-02-23 15:06:46 -0500

    Routine updates.

--- 20250222t0.sh
+++ 20250222t0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenCT -d 701 --name Default
-python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenCT -d 701 -p nnUNetResEncUNetMPlans --name ResEncUNetM
+python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenCT -d 701 -c 3d_fullres --name Default
+python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenCT -d 701 -p nnUNetResEncUNetMPlans -c 3d_fullres --name ResEncUNetM
--- 20250222t1.sh
+++ 20250222t1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenMR -d 702 --name Default --save_path /workspace/data/bwlab_eval_results_2.csv --eval_input /workspace/data/nnUNet_eval_input_2 --eval_output /workspace/data/nnUNet_eval_output_2
-python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenMR -d 702 -p nnUNetResEncUNetMPlans --name ResEncUNetM --save_path /workspace/data/bwlab_eval_results_2.csv --eval_input /workspace/data/nnUNet_eval_input_2 --eval_output /workspace/data/nnUNet_eval_output_2
+python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenMR -d 702 -c 3d_fullres --name Default --save_path /workspace/data/bwlab_eval_results_2.csv --eval_input /workspace/data/nnUNet_eval_input_2 --eval_output /workspace/data/nnUNet_eval_output_2
+python /workspace/code/main.py -tr nnUNetTrainerMICCAI -dn AbdomenMR -d 702 -c 3d_fullres -p nnUNetResEncUNetMPlans --name ResEncUNetM --save_path /workspace/data/bwlab_eval_results_2.csv --eval_input /workspace/data/nnUNet_eval_input_2 --eval_output /workspace/data/nnUNet_eval_output_2
