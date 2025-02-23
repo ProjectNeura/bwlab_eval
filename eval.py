3d8e15d1e331a9d9a069454761a309f887cddadc
ATATC <futerry@outlook.com>
2025-02-22 21:41:49 -0500

    Routine updates.

--- eval.py
+++ eval.py
@@ -54,11 +54,11 @@ def __entry__() -> None:
                 dsc = compute_dice_coefficient(organ_i_gt, organ_i_pred)
             seg_metrics[LABELS[i - 1]].append(dsc := round(dsc, 4))
             seg_metrics["mean"].append(dsc)
-        df = {k: [sum(v) / len(v)] for k, v in seg_metrics.items()}
-        if args.name:
-            df["name"] = [args.name]
-        df = _DataFrame(df)
-        df.to_csv(args.save_path, index=False, mode="a" if args.name else "w", header=False)
+    df = {k: [sum(v) / len(v)] for k, v in seg_metrics.items()}
+    if args.name:
+        df["name"] = [args.name]
+    df = _DataFrame(df)
+    df.to_csv(args.save_path, index=False, mode="a" if args.name else "w", header=False)
 
 
 if __name__ == "__main__":
